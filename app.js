493d7c7ba29f655ae32560fc8c63395d7879e1b5
Bryton Salisbury <brytonsalisbury@gmail.com>
2020-04-27 21:54:43 +0930

    Created login page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const index = require('./routes/index.js');
 
 const app = express();
+
 
 const port = 8080;
 
 app.use(morgan('dev'));
+app.use('/', index);
 app.use(express.static('public'));
 
 
 app.listen(port, () => {
-  console.debug('App listening on port: ' + port);
+  // eslint-disable-next-line no-console
+  console.debug(`App listening on port: ${port}`);
 });
-
